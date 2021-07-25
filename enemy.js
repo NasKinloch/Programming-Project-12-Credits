35216a68ed0e35929eb3fdac7956253d82e08e10
NasKinloch <51344359+NasKinloch@users.noreply.github.com>
2021-07-26 09:30:55 +1200

    Enemy Upload

---
+++ enemy.js
@@ -0,0 +1,33 @@
+class Enemy {
+    constructor(x, y, w, h, c, xSpeed, ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.c = c;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    drawRect() {
+        canvasContext.fillStyle = this.c;
+        canvasContext.fillRect(this.x, this.y, this.w, this.h);
+
+    }
+
+    move() {
+//        this.x += this.xSpeed;
+        this.y += this.ySpeed;
+
+        if (this.x + this.w >= canvas.width || this.x <= 0) {
+            this.xSpeed *= -1;
+        }
+
+        if(this.y >canvas.height){
+            this.y = 0 - this.h;
+            this.ySpeed = Math.floor(Math.random() * (6 - 3) + 3);
+            score++;
+//            console.log('score:' + score);
+        }
+    }
+}
