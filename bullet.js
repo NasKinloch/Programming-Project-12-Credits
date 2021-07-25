acfa4e4cb030b0f28e677d3220ac3a65fddcbd0e
NasKinloch <51344359+NasKinloch@users.noreply.github.com>
2021-07-26 09:30:03 +1200

    Bullet Upload

---
+++ bullet.js
@@ -0,0 +1,47 @@
+class Bullet {
+    constructor(x, y, w, h, c, ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.c = c;
+        this.ySpeed = ySpeed;
+    }
+
+    drawRect() {
+        canvasContext.fillStyle = this.c;
+        canvasContext.fillRect(this.x, this.y, this.w, this.h);
+
+    }
+
+    move() {
+        this.y -= this.ySpeed;
+    }
+
+
+    outOfBounds() {
+        return this.y < 0;
+    }
+
+    hasHitItem(item) {
+        return ((this.x + this.w) >= item.x && this.x <= (item.x + item.w)) && ((this.y + this.h) >= item.y && this.y <= (item.y + item.h));
+    }
+
+    hasHitEnemy(enemy) {
+        return this.hasHitItem(enemy);
+    }
+
+    hasCollided() {
+        var self = this;
+        var collided = false;
+        enemies.forEach(function (enemy, i) {
+            if (self.hasHitEnemy(enemy)) {
+                delete enemies[i];
+                collided = true;
+            }
+        });
+        enemies = enemies.filter(item => item !== undefined);
+         return collided;
+    }
+
+}
